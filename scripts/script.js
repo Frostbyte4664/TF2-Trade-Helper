5e83b4e69290f41ea7b91d03025002e523427c98
Frostbyte <72130297+Frostbyte4664@users.noreply.github.com>
2020-10-03 09:13:04 -0400

    Automatically close warnings

--- scripts/script.js
+++ scripts/script.js
@@ -1,4 +1,5 @@
 function endTutorial() {
+    //Closes the tutorial if it can be found
     if (window.eval("Tutorial")) {
         window.eval("Tutorial.EndTutorial()")
         console.log("Closed the tutorial.")
@@ -9,13 +10,25 @@ function endTutorial() {
 }
 
 function selectGame() {
+    //Selects Team Fortress 2 from the game inventories
     window.eval("TradePageSelectInventory(g_ActiveUser, 440, '2');");
-    console.log("Selected Team Fortress 2 inventory");
+    console.log("Selected Team Fortress 2 inventory.");
+}
+
+function disableWarnings() {
+    //Closes any initial warnings and disables future warning flags
+    window.eval("CModal.DismissActiveModal();");
+    window.eval("g_bWarnedAboutPlaytime = false;");
+    window.eval("g_bWarnedAboutUnvettedApp = false;");
+    window.eval("g_bWarnOnReady = false;");
+    console.log("Disabled trade warnings.");
 }
 
 function onLoad() {
+    //Called when the page is loaded
     endTutorial();
     selectGame();
+    disableWarnings();
 }
 
 document.addEventListener("DOMContentLoaded", onLoad());
