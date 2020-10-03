c85ae62a0d56ade56ef4d5acbea9fc01c978432c
Frostbyte <72130297+Frostbyte4664@users.noreply.github.com>
2020-10-03 08:22:41 -0400

    Added tutorial auto-closer

--- scripts/script.js
+++ scripts/script.js
@@ -1,1 +1,15 @@
-console.log("TEST");
+function endTutorial() {
+    if (window.eval("Tutorial")) {
+        window.eval("Tutorial.EndTutorial()")
+        console.log("Closed the tutorial.")
+    }
+    else {
+        console.log("Could not find Tutorial object.");
+    }
+}
+
+function onLoad() {
+    endTutorial();
+}
+
+document.addEventListener("DOMContentLoaded", onLoad());
