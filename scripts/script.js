401761b02ac931ed25767d00e1c81289f2827625
Frostbyte <72130297+Frostbyte4664@users.noreply.github.com>
2020-10-03 08:35:06 -0400

    Added automatic game selector

--- scripts/script.js
+++ scripts/script.js
@@ -8,8 +8,14 @@ function endTutorial() {
     }
 }
 
+function selectGame() {
+    window.eval("TradePageSelectInventory(g_ActiveUser, 440, '2');");
+    console.log("Selected Team Fortress 2 inventory");
+}
+
 function onLoad() {
     endTutorial();
+    selectGame();
 }
 
 document.addEventListener("DOMContentLoaded", onLoad());
