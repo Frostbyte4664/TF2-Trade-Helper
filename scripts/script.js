07b81a09fc803743ff5a535df0177a58247db7be
Frostbyte <72130297+Frostbyte4664@users.noreply.github.com>
2020-10-25 18:31:47 -0400

    Added automatic metal counter

--- scripts/script.js
+++ scripts/script.js
@@ -11,8 +11,9 @@ function endTutorial() {
 
 function selectGame() {
     //Selects Team Fortress 2 from the game inventories
-    window.eval("TradePageSelectInventory(g_ActiveUser, 440, '2');");
-    console.log("Selected Team Fortress 2 inventory.");
+    window.eval("TradePageSelectInventory(UserThem, 440, 2);");
+    window.eval("TradePageSelectInventory(UserYou, 440, 2);");
+    console.log("Selected and loaded Team Fortress 2 inventory.");
 }
 
 function disableWarnings() {
@@ -40,16 +41,54 @@ function onLoad() {
 
 document.addEventListener("DOMContentLoaded", onLoad());
 
+const itemName = new window.Function("user", "num", "return user.getInventory(440, 2).rgItemElements[num].rgItem.name")
+
+function countMetalInTrade(user) {
+    var userObject = window.eval(user);
+    let inventory = window.eval(user + ".getInventory(440, 2).rgItemElements");
+    let scraps = 0;
+    for (var i = inventory.length-1; i >= 0; i--) {
+        if (inventory[i].firstChild) {
+            if (inventory[i].firstChild.style.display == "none") {
+                switch (itemName(userObject, i)) {
+                    case "Refined Metal":
+                        scraps += 9;
+                        break;
+                    case "Reclaimed Metal":
+                        scraps += 3;
+                        break;
+                    case "Scrap Metal":
+                        scraps += 1;
+                        break;
+                }
+            }
+        }
+    }
+    return scraps;
+}
+
+function toRef(scrap) {
+    //let result = scrap % 9;
+    //result += Math.round((scrap - result*9)*11)/100;
+    let result = scrap;
+    let bob = scrap % 9;
+    result -= bob;
+    result /= 9;
+    bob = bob * 0.11;
+    return (Math.trunc((result + bob) * 100) / 100);
+}
+
 function itemsChanged(mutationList, observer) {
     //When the items in the change trade
     if (document.getElementById("your_slots").contains(mutationList[0].target)) {
-        //Recount metal in my inventory
+        //Count my metal in trade
+        document.getElementById("trade_yours").querySelector("h2").innerText = toRef(countMetalInTrade("UserYou")) + " ref";
     }
-    else {
-        //Recount metal in their inventory
+    else if (document.getElementById("their_slots").contains(mutationList[0].target)){
+        //Count their metal in trade
+        document.getElementById("trade_theirs").querySelectorAll("h2")[1].innerText = toRef(countMetalInTrade("UserThem")) + " ref";
     }
 }
 
 const observer = new MutationObserver(itemsChanged);
-
 observer.observe(document.getElementById("trade_area"), { subtree: true, childList: true });
