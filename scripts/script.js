6ed04ccc19326ca10133ab4d2be5c7a795ed6a78
Frostbyte <72130297+Frostbyte4664@users.noreply.github.com>
2020-11-01 20:34:34 -0500

    Cleaned up and improved code

--- scripts/script.js
+++ scripts/script.js
@@ -1,94 +1,66 @@
-function endTutorial() {
-    //Closes the tutorial if it can be found
-    if (window.eval("Tutorial")) {
-        window.eval("Tutorial.EndTutorial()")
-        console.log("Closed the tutorial.")
-    }
-    else {
-        console.log("Could not find Tutorial object.");
-    }
-}
+const getInventory = new window.Function("user", "return user.getInventory(440, 2).rgItemElements")
+const getItemFromElement = new window.Function("element", "return element.rgItem")
+const getUser = new window.Function("isThem", "if (isThem){return UserThem} return UserYou")
 
-function selectGame() {
-    //Selects Team Fortress 2 from the game inventories
-    window.eval("TradePageSelectInventory(UserThem, 440, 2);");
-    window.eval("TradePageSelectInventory(UserYou, 440, 2);");
-    console.log("Selected and loaded Team Fortress 2 inventory.");
-}
+const countMetal = new Function("items", "return items.ref * 9 + items.rec * 3 + items.scrap")
+const plural = new Function("name", "count", "if (count!=1) {return count + ' ' + name+'s'} return count + ' ' + name")
+const toRef = new Function("scrap", "return Math.round(((scrap % 9 * 0.11) + (scrap - scrap % 9) / 9) * 100) / 100")
 
-function disableWarnings() {
-    //Closes any initial warnings and disables future warning flags
-    window.eval("CModal.DismissActiveModal();");
-    window.eval("g_bWarnedAboutPlaytime = false;");
-    window.eval("g_bWarnedAboutUnvettedApp = false;");
-    window.eval("g_bWarnOnReady = false;");
-    console.log("Disabled trade warnings.");
-}
-
-function hideInfo() {
-    //Removes the trade partner information at the top of the window
-    document.body.getElementsByClassName('trade_partner_header')[0].remove();
-    console.log("Removed trade partner info.")
-}
-
-function onLoad() {
-    //Called when the page is loaded
-    endTutorial();
-    selectGame();
-    disableWarnings();
-    hideInfo();
-}
-
-document.addEventListener("DOMContentLoaded", onLoad());
-
-const itemName = new window.Function("user", "num", "return user.getInventory(440, 2).rgItemElements[num].rgItem.name")
-
-function countMetalInTrade(user) {
-    var userObject = window.eval(user);
-    let inventory = window.eval(user + ".getInventory(440, 2).rgItemElements");
-    let scraps = 0;
-    for (var i = inventory.length-1; i >= 0; i--) {
-        if (inventory[i].firstChild) {
-            if (inventory[i].firstChild.style.display == "none") {
-                switch (itemName(userObject, i)) {
-                    case "Refined Metal":
-                        scraps += 9;
-                        break;
-                    case "Reclaimed Metal":
-                        scraps += 3;
-                        break;
-                    case "Scrap Metal":
-                        scraps += 1;
-                        break;
-                }
+function countItems(inventory, inTrade) {
+    let result = { keys: 0, ref: 0, rec: 0, scrap: 0, items: 0 }
+    for (var item of inventory) {
+        if (inTrade == (item.firstChild.style.display == "none")) {
+            switch (getItemFromElement(item).name) {
+                case "Mann Co. Supply Crate Key":
+                    result.keys++
+                    break
+                case "Refined Metal":
+                    result.ref++
+                    break
+                case "Reclaimed Metal":
+                    result.rec++
+                    break
+                case "Scrap Metal":
+                    result.scrap++
+                    break
+                default:
+                    result.items++
+                    break
             }
         }
     }
-    return scraps;
+    return result
 }
 
-function toRef(scrap) {
-    //let result = scrap % 9;
-    //result += Math.round((scrap - result*9)*11)/100;
-    let result = scrap;
-    let bob = scrap % 9;
-    result -= bob;
-    result /= 9;
-    bob = bob * 0.11;
-    return (Math.trunc((result + bob) * 100) / 100);
-}
-
-function itemsChanged(mutationList, observer) {
-    //When the items in the change trade
+function itemsChanged(mutationList) {
     if (document.getElementById("your_slots").contains(mutationList[0].target)) {
-        //Count my metal in trade
-        document.getElementById("trade_yours").querySelector("h2").innerText = toRef(countMetalInTrade("UserYou")) + " ref";
+        let items = countItems(getInventory(getUser()), true)
+        document.getElementById("trade_yours").querySelector("h2").innerText = plural("key", items.keys) + ", " + toRef(countMetal(items)) + " ref" + ", " + plural("item", items.items)
     }
-    else if (document.getElementById("their_slots").contains(mutationList[0].target)){
-        //Count their metal in trade
-        document.getElementById("trade_theirs").querySelectorAll("h2")[1].innerText = toRef(countMetalInTrade("UserThem")) + " ref";
+    else if (document.getElementById("their_slots").contains(mutationList[0].target)) {
+        let items = countItems(getInventory(getUser(1)), true)
+        document.getElementById("trade_theirs").querySelectorAll("h2")[1].innerText = plural("key", items.keys) + ", " + toRef(countMetal(items)) + " ref" + ", " + plural("item", items.items)
     }
 }
 
-const observer = new MutationObserver(itemsChanged);
-observer.observe(document.getElementById("trade_area"), { subtree: true, childList: true });
+function onLoad() {
+    let endTutorial = new window.Function("Tutorial.EndTutorial()")
+    let selectGame = new window.Function("SelectInventory(440, 2)")
+    let hideInfo = new Function("document.querySelector('.trade_partner_header').remove()")
+
+    let disableWarnings = new window.Function("CModal.DismissActiveModal();" +
+        "ToggleReady = function (ready) { UserYou.bReady = ready;" +
+        "GTradeStateManager.ToggleReady(ready);" +
+        "UpdateReadyButtons();" +
+        "document.getElementById('notready_tradechanged_message').style.display = 'none' }")
+
+    endTutorial()
+    selectGame()
+    hideInfo()
+    disableWarnings()
+
+    const observer = new MutationObserver(itemsChanged)
+    observer.observe(document.getElementById("trade_area"), { subtree: true, childList: true })
+}
+
+document.addEventListener("DOMContentLoaded", onLoad())
