c299d97bc8f6916f8754d0c4485cd9bcb82c6552
Frostbyte <72130297+Frostbyte4664@users.noreply.github.com>
2020-11-03 03:00:06 -0500

    Added inventory page navigation with A and D keys

--- scripts/script.js
+++ scripts/script.js
@@ -6,6 +6,10 @@ const countMetal = new Function("items", "return items.ref * 9 + items.rec * 3 +
 const plural = new Function("name", "count", "if (count!=1) {return count + ' ' + name+'s'} return count + ' ' + name")
 const toRef = new Function("scrap", "return Math.round(((scrap % 9 * 0.11) + (scrap - scrap % 9) / 9) * 100) / 100")
 
+const inventoryScroll = new window.Function("e", "if (e.target == document.body) {" +
+    "if (e.key == 'a') {InventoryPreviousPage()}" +
+    "else if (e.key == 'd') {InventoryNextPage()}}")
+
 function countItems(inventory, inTrade) {
     let result = { keys: 0, ref: 0, rec: 0, scrap: 0, items: 0 }
     for (var item of inventory) {
@@ -61,6 +65,8 @@ function onLoad() {
 
     const observer = new MutationObserver(itemsChanged)
     observer.observe(document.getElementById("trade_area"), { subtree: true, childList: true })
+
+    document.addEventListener("keydown", inventoryScroll)
 }
 
 document.addEventListener("DOMContentLoaded", onLoad())
