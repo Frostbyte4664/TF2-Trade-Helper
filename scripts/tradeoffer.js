30421869e27b36ff94387d895e6a264b539ae3e4
Frostbyte <72130297+Frostbyte4664@users.noreply.github.com>
2020-12-18 19:22:30 -0500

    Completely rewrote tradeoffer.js

--- scripts/tradeoffer.js
+++ scripts/tradeoffer.js
@@ -1,107 +1,148 @@
-const addItem = new window.Function("elItem", "MoveItemToTrade(elItem)")
-const cancelHover = new window.Function("elItem", "CancelItemHover(elItem)")
-const clearDrags = new window.Function("for (x of Draggables.drags){x.destroy()}")
-const getInventory = new window.Function("user", "return user.getInventory(440, 2).rgItemElements")
-const getItemFromElement = new window.Function("element", "return element.rgItem")
-const getUser = new window.Function("isThem", "if (isThem){return UserThem} return UserYou")
-const removeItem = new window.Function("elItem", "MoveItemToInventory(elItem)")
-
-const countMetal = function (items) { return items.ref * 9 + items.rec * 3 + items.scrap }
-const plural = function (name, count) { if (count != 1) { return count + ' ' + name + 's' } return count + ' ' + name }
-const toRef = function (scrap) { return Math.round(((scrap % 9 * 0.11) + (scrap - scrap % 9) / 9) * 100) / 100 }
-
-const hotkeys = new window.Function("e", "if ((e.target != document.querySelector('#filter_control')) &&" +
-    "(e.target != document.querySelector('#trade_offer_note'))) { " +
-    "if (e.key == 'a') {InventoryPreviousPage()}" +
-    "else if (e.key == 'd') {InventoryNextPage()}" +
-    "else if (e.key == 'c') {ToggleReady(!UserYou.bReady)}" +
-    "else if (e.key == 'v') {ConfirmTradeOffer()}" +
-    "else if (e.key == 'z') {DeclineTradeOffer()}" +
-    "else if (e.key == 't') {DismissTradeOfferWindow()}" +
-    "else if (e.key == 'q') {if (g_ActiveUser == UserYou) {" +
-    "SelectInventoryFromUser(UserThem, g_ActiveInventory.appid, g_ActiveInventory.contextid)}" +
-    "else {SelectInventoryFromUser(UserYou, g_ActiveInventory.appid, g_ActiveInventory.contextid)}" +
-    "UpdateDisplayForActiveUser()}}")
-
-function countItems(inventory, inTrade) {
-    let result = { keys: 0, ref: 0, rec: 0, scrap: 0, items: 0 }
-    for (var item of inventory) {
-        if (inTrade == (item.firstChild.style.display == "none")) {
-            switch (getItemFromElement(item).name) {
+function w(func) {
+    //w(function) returns the same function redefined with a window.Function constructor.
+    //This allows functions to access variable defined in the window.
+    fstr = func.toString();
+    //Get arguments from the function in string form
+    argstr = (fstr.substring(
+        fstr.indexOf("(") + 1,
+        fstr.indexOf(")")));
+    //Remove whitespace from the result
+    argstr = argstr.replace(/\s/g, "");
+    //Make array of the function's arguments
+    args = [];
+    while (argstr.length > 0) {
+        if (argstr.includes(",")) {
+            args.push(argstr.substring(0, argstr.indexOf(",")));
+            argstr = argstr.substring(argstr.indexOf(",") + 1);
+        }
+        else {
+            args.push(argstr);
+            argstr = "";
+        }
+    }
+    //Get a string of the function body
+    fstr = fstr.substring(
+        fstr.indexOf("{") + 1,
+        fstr.lastIndexOf("}"));
+    return window.Function(...args, fstr);
+}
+const hotkeys = w(function (e) {
+    if (e.target != document.querySelector("#filter_control") && e.target != document.querySelector("trade_offer_note")) {
+        if (e.key == "a") { InventoryPreviousPage(); }
+        else if (e.key == "d") { InventoryNextPage(); }
+        else if (e.key == "c") { ToggleReady(!UserYou.bReady); }
+        else if (e.key == "v") { ConfirmTradeOffer(); }
+        else if (e.key == "z") { DeclineTradeOffer(); }
+        else if (e.key == "t") { DismissTradeOfferWindow(); }
+        else if (e.key == "q") {
+            if (g_ActiveUser == UserYou) { //Switches between your inventory and their inventory
+                SelectInventoryFromUser(UserThem, g_ActiveInventory.appid, g_ActiveInventory.contextid);
+            }
+            else {
+                SelectInventoryFromUser(UserYou, g_ActiveInventory.appid, g_ActiveInventory.contextid);
+            }
+            UpdateDisplayForActiveUser();
+        }
+    }
+});
+const itemClick = w(function (e) {
+    if (!g_bReadOnly) {
+        let elItem = e.target.parentElement;
+        if (e.buttons == 1 && elItem && elItem.rgItem) {
+            for (x of Draggables.drags) {
+                x.destroy();
+            }
+            if (document.querySelector("#inventory_box").contains(elItem)) {
+                MoveItemToTrade(elItem);
+            }
+            else {
+                MoveItemToInventory(elItem);
+            }
+            CancelItemHover(elItem);
+        }
+    }
+});
+const countItems = w(function (user, inTrade) {
+    let inventory;
+    if (user == "you") {
+        inventory = UserYou.getInventory(440, 2).rgItemElements;
+    }
+    else {
+        inventory = UserThem.getInventory(440, 2).rgItemElements;
+    }
+    let inv = { keys: 0, ref: 0, rec: 0, scrap: 0, items: 0 }
+    for (var elItem of inventory) {
+        if (inTrade == (elItem.firstChild.style.display == "none")) { //If looking looking for items in trade and item is in trade
+            //Or if looking for items NOT in trade and item is NOT in trade
+            switch (elItem.rgItem.name) {
                 case "Mann Co. Supply Crate Key":
-                    result.keys++
-                    break
+                    inv.keys++;
+                    break;
                 case "Refined Metal":
-                    result.ref++
-                    break
+                    inv.ref++;
+                    break;
                 case "Reclaimed Metal":
-                    result.rec++
-                    break
+                    inv.rec++;
+                    break;
                 case "Scrap Metal":
-                    result.scrap++
-                    break
+                    inv.scrap++;
+                    break;
                 default:
-                    result.items++
-                    break
+                    inv.items++;
+                    break;
             }
         }
     }
-    return result
-}
-
-function clickItem(e) {
-    let elItem = e.target.parentElement
-    let item = getItemFromElement(elItem)
-    if (e.buttons == 1 && item) {
-        clearDrags()
-        if (document.querySelector("#inventory_box").contains(elItem)) {
-            addItem(elItem)
-        }
-        else {
-            removeItem(elItem)
-        }
-        cancelHover(elItem)
+    return inv;
+});
+function countMetal(inv) { return inv.ref * 9 + inv.rec * 3 + inv.scrap }
+function toRef(scrap) { return Math.trunc(scrap / 9 * 100) / 100 }
+function plural(name, count) { if (count != 1) { return count + ' ' + name + 's' } return count + ' ' + name }
+function updateItemCount(user) {
+    let inv = countItems(user, true);
+    text = plural("key", inv.keys) + ", " + toRef(countMetal(inv)) + " ref" + ", " + plural("item", inv.items);
+    if (user == "you") {
+        document.getElementById("trade_yours").querySelector("h2").innerText = text;
+    }
+    else {
+        document.getElementById("trade_theirs").querySelectorAll("h2")[1].innerText = text;
     }
 }
-
 function itemsChanged(mutationList) {
-    if (document.getElementById("your_slots").contains(mutationList[0].target)) {
-        let items = countItems(getInventory(getUser()), true)
-        document.getElementById("trade_yours").querySelector("h2").innerText = plural("key", items.keys) + ", " + toRef(countMetal(items)) + " ref" + ", " + plural("item", items.items)
+    if (document.getElementById("your_slots").contains(mutationList[0].target)) { //If items changed in your inventory
+        updateItemCount("you");
     }
-    else if (document.getElementById("their_slots").contains(mutationList[0].target)) {
-        let items = countItems(getInventory(getUser(1)), true)
-        document.getElementById("trade_theirs").querySelectorAll("h2")[1].innerText = plural("key", items.keys) + ", " + toRef(countMetal(items)) + " ref" + ", " + plural("item", items.items)
+    else if (document.getElementById("their_slots").contains(mutationList[0].target)) { //If items changed in their inventory
+        updateItemCount("them");
     }
 }
-
 function onLoad() {
-    let endTutorial = new window.Function("Tutorial.EndTutorial()")
-    let selectGame = new window.Function("SelectInventory(440, 2)")
-    let readOnly = new window.Function("return g_bReadOnly")
-    let hideInfo = function () { document.querySelector('.trade_partner_header').remove() }
-
-    let disableWarnings = new window.Function("CModal.DismissActiveModal();" +
-        "ToggleReady = function (ready) { UserYou.bReady = ready;" +
-        "GTradeStateManager.ToggleReady(ready);" +
-        "UpdateReadyButtons();" +
-        "document.getElementById('notready_tradechanged_message').style.display = 'none' }")
-
-    endTutorial()
-    selectGame()
-    hideInfo()
-    disableWarnings()
-
-    const observer = new MutationObserver(itemsChanged)
-    observer.observe(document.getElementById("trade_area"), { subtree: true, childList: true })
-
-    document.addEventListener("keydown", hotkeys)
-
-    if (!readOnly()) {
-        document.querySelector("style").sheet.insertRule("#your_slots .item a.inventory_item_link, " +
-            "#inventories .item a.inventory_item_link {cursor: pointer}", 0)
-        document.addEventListener("pointerdown", clickItem)
-    }
+    let wOnLoad = w(function () { //For operations that use window variables
+        Tutorial.EndTutorial(); //Close tutorial
+        SelectInventory(440, 2); //Select TF2 Inventory
+        document.querySelector('.trade_partner_header').remove(); //Hides trade parter info at the top of the screen
+        //.remove() is redefined for some reason and must be called in the window
+        //Disable trade warnings
+        CModal.DismissActiveModal();
+        ToggleReady = function (ready) {
+            UserYou.bReady = ready;
+            GTradeStateManager.ToggleReady(ready);
+            UpdateReadyButtons();
+            document.getElementById('notready_tradechanged_message').style.display = 'none';
+        }
+    });
+    wOnLoad();
+    document.addEventListener("keydown", hotkeys); //Initiate hotkeys
+    //Set up single click item add/remove
+    document.querySelector("style").sheet.insertRule("#your_slots .item a.inventory_item_link, " +
+        "#inventories .item a.inventory_item_link {cursor: pointer}", 0); //Change cursor to pointer when hovering item
+    document.addEventListener("pointerdown", itemClick);
+    //Create observer to watch for item changes in the trade
+    new MutationObserver(itemsChanged).observe(document.getElementById("trade_area"), { subtree: true, childList: true });
+    //Make sure the metal/item counter updates after loading
+    setTimeout(function () {
+        updateItemCount("you");
+        updateItemCount("them");
+    }, 3000);
 }
-
-document.addEventListener("DOMContentLoaded", onLoad())
+document.addEventListener("DOMContentLoaded", onLoad());
